3d15febeb3059e52a91c1c39e18ac0c0e262f431
nickick <hong.nick@gmail.com>
2022-06-29 18:27:01 -0400

    Add mint buttons

--- src/ImageZoom.tsx
+++ src/ImageZoom.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import gsap from 'gsap';
 import React, { useEffect, useRef } from 'react';
 
@@ -41,6 +41,7 @@ const resetAnimation: AnimationSettings = {
 const ImageZoom: React.FC<Props> = ({ imgSrc, imgAlt, colorType }) => {
   const shadowRef = useRef<HTMLDivElement>(null);
   const imageRef = useRef<HTMLImageElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const onMouseEnter = () => {
@@ -57,15 +58,19 @@ const ImageZoom: React.FC<Props> = ({ imgSrc, imgAlt, colorType }) => {
       }
     };
 
-    if (imageRef.current) {
+    if (imageRef.current && buttonRef.current) {
       imageRef.current.addEventListener('mouseenter', onMouseEnter);
       imageRef.current.addEventListener('mouseleave', onMouseLeave);
+      buttonRef.current.addEventListener('mouseenter', onMouseEnter);
+      buttonRef.current.addEventListener('mouseleave', onMouseLeave);
     }
 
     return () => {
-      if (imageRef.current) {
+      if (imageRef.current && buttonRef.current) {
         imageRef.current.removeEventListener('mouseenter', onMouseEnter);
         imageRef.current.removeEventListener('mouseleave', onMouseLeave);
+        buttonRef.current.removeEventListener('mouseenter', onMouseEnter);
+        buttonRef.current.removeEventListener('mouseleave', onMouseLeave);
       }
     };
   }, [shadowRef.current, imageRef.current]);
@@ -73,48 +78,77 @@ const ImageZoom: React.FC<Props> = ({ imgSrc, imgAlt, colorType }) => {
   return (
     <Box
       sx={{
+        flexDirection: 'column',
+        alignItems: 'center',
         flex: 1,
         width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        py: 30,
-        position: 'relative',
         background: colorType === 'light' ? '#efefef' : '#141414',
         overflow: 'hidden',
+        pt: 30,
+        pb: 8,
       }}
     >
       <Box
         sx={{
-          background: colorType === 'light' ? '#dadada' : '#212121',
-          width: '580px',
-          height: '580px',
-          position: 'absolute',
-          borderRadius: '50%',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-        }}
-        ref={shadowRef}
-      />
-      <Box
-        style={{
-          border: `${colorType === 'light' ? '#fff' : '#000'} 0.5rem solid`,
-          width: '70%',
+          flex: 1,
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
           position: 'relative',
-          overflow: 'hidden',
-          filter: 'drop-shadow(8rem 8rem 144px #000000)',
+          mb: 17,
         }}
       >
-        <img
-          src={imgSrc}
-          alt={imgAlt}
-          ref={imageRef}
-          style={{
-            width: '100%',
-            display: 'flex',
-            transform: 'scale(1.1)',
+        <Box
+          sx={{
+            background: colorType === 'light' ? '#dadada' : '#212121',
+            width: '580px',
+            height: '580px',
+            position: 'absolute',
+            borderRadius: '50%',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
           }}
+          ref={shadowRef}
         />
+        <Box
+          sx={{
+            border: `${colorType === 'light' ? '#fff' : '#000'} 0.5rem solid`,
+            width: '70%',
+            position: 'relative',
+            overflow: 'hidden',
+            filter: 'drop-shadow(8rem 8rem 144px #000000)',
+          }}
+        >
+          <img
+            src={imgSrc}
+            alt={imgAlt}
+            ref={imageRef}
+            style={{
+              width: '100%',
+              display: 'flex',
+              transform: 'scale(1.1)',
+            }}
+          />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          position: 'relative',
+          zIndex: '2',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          ref={buttonRef}
+        >
+          <Typography variant="body2" color="secondary">Mint &#40;0.05&#41;</Typography>
+        </Button>
       </Box>
     </Box>
   );
--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,33 @@
 import { createTheme } from '@mui/material';
 
+const { palette } = createTheme();
+const { augmentColor } = palette;
+
+const createColor = (mainColor: string) => augmentColor({ color: { main: mainColor } });
+
 const theme = createTheme({
   typography: {
     h1: {
       fontFamily: 'Circular Std Bold',
       fontSize: '2.5rem',
     },
-    body1: {
-      fontFamily: 'Freight, serif',
-      fontSize: '3.125rem',
+    body2: {
+      fontFamily: 'Lato, sans-serif',
+      fontSize: '1.5rem',
+      lineHeight: '2remm',
+    },
+  },
+  palette: {
+    primary: createColor('#000'),
+    secondary: createColor('#fff'),
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '0',
+        },
+      },
     },
   },
 });
