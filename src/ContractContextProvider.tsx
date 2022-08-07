e04036cc4f05a5558b1990ef939299a15985696c
nickick <hong.nick@gmail.com>
2022-08-07 11:12:29 -0400

    Add dotenv, contract address, and reading contract status

--- src/ContractContextProvider.tsx
+++ src/ContractContextProvider.tsx
@@ -1,10 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 /* eslint-disable react/jsx-no-constructed-context-values */
+import { ethers } from 'ethers';
 import React, { createContext, useEffect, useState } from 'react';
+import contractAbi from './contractAbi.json';
+
+// eslint-disable-next-line no-shadow
+enum ContractStatus {
+  Paused,
+  Premint,
+  Mint
+}
 
 interface ContextInterface {
   currentAccount: string | null;
   errorMessage: string | null;
   connectWallet: () => void;
+  getContractStatus: () => Promise<ContractStatus>;
+  contractStatus: ContractStatus;
 }
 
 export const ContractContext = createContext<ContextInterface>({} as ContextInterface);
@@ -16,6 +29,7 @@ type Props = {
 const ContractContextProvider = ({ children }: Props) => {
   const [currentAccount, setCurrentAccount] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
+  const [contractStatus, setContractStatus] = useState(0);
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -59,12 +73,52 @@ const ContractContextProvider = ({ children }: Props) => {
     }
   };
 
+  const contractAddress = process.env.NEXT_PUBLIC_SIGNS_CONTRACT_ADDRESS || '';
+
+  function getProvider() {
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    return provider;
+  }
+
+  // fetch NYC365 Contract
+  const getSignsContract = () => {
+    const provider = getProvider();
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+
+    return contract;
+  };
+
+  function handleError(error: unknown) {
+    let message = 'Unknown Error';
+    if (error instanceof Error) message = error.message;
+    console.error(error);
+    setErrorMessage(message);
+  }
+
+  const getContractStatus = async (): Promise<ContractStatus> => {
+    const { ethereum } = window;
+
+    try {
+      if (!ethereum) {
+        // eslint-disable-next-line no-alert
+        alert('Please install MetaMask or another wallet provider.');
+        return ContractStatus.Paused;
+      }
+      const signsContract = getSignsContract();
+      const status = await signsContract.contractStatus();
+      setContractStatus(status);
+      return contractStatus;
+    } catch (error) {
+      handleError(error);
+      return ContractStatus.Paused;
+    }
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
-
-    return () => {
-      console.log('test');
-    };
+    getContractStatus();
   }, [currentAccount]);
 
   return (
@@ -72,6 +126,8 @@ const ContractContextProvider = ({ children }: Props) => {
       currentAccount,
       connectWallet,
       errorMessage,
+      getContractStatus,
+      contractStatus,
     }}
     >
       {children}
